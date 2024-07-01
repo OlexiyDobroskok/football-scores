05618a3df72345966974ee1e459a3b29d27661b4
Oleksii Dobroskok <a.v.dobroskok.kh@gmail.com>
2024-07-01 20:57:11 +0300

    Correct Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: ["./app/**/*.tsx"],
+  content: ['./src/**/*.tsx'],
   theme: {},
   plugins: [],
 };
