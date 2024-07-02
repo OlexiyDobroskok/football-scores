de6dd8d7b5043a21df30b7f5ee7d75a709a7bf36
Oleksii Dobroskok <a.v.dobroskok.kh@gmail.com>
2024-07-02 14:37:20 +0300

    Add Base color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,23 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: ['./src/**/*.tsx'],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        accent: 'hsl(var(--accent))',
+      },
+    },
+  },
   plugins: [],
 };
 export default config;
