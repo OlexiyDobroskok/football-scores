4f3d8ca84df51700d326b0fa1da579e824ddc978
Oleksii Dobroskok <a.v.dobroskok.kh@gmail.com>
2024-07-08 19:06:31 +0300

    Add isSingle prop to the Tab component

--- src/shared/ui/tab/tab.tsx
+++ src/shared/ui/tab/tab.tsx
@@ -6,20 +6,22 @@ export interface TabProps {
   isActive: boolean;
   children: React.ReactNode;
   className?: string;
+  isSingle?: boolean;
 }
 
-export function Tab({ className, isActive, children }: TabProps) {
+export function Tab({ className, isActive, isSingle, children }: TabProps) {
   return (
     <div className={cn(className, 'space-y-2 pt-3')}>
       <div>{children}</div>
       <div
         className={cn('h-1 bg-accent/10 px-0.5 transition-all', {
-          'bg-transparent': isActive,
+          'bg-transparent': isActive && !isSingle,
         })}
       >
         <div
           className={cn('mx-auto h-1 w-0 rounded transition-all', {
-            'w-full bg-accent shadow-md shadow-accent/60': isActive,
+            'w-full bg-accent shadow-md shadow-accent/60':
+              isActive && !isSingle,
           })}
         ></div>
       </div>
