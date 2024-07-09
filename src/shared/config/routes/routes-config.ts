dfab0b800795173bf975192c4e76570ea014b1fa
Oleksii Dobroskok <a.v.dobroskok.kh@gmail.com>
2024-07-09 22:49:58 +0300

    Add Team route

--- src/shared/config/routes/routes-config.ts
+++ src/shared/config/routes/routes-config.ts
@@ -2,6 +2,7 @@ export const appRoutes = {
   HOME: '/',
   EXPLORE: '/explore',
   MATCH: '/match',
+  TEAM: '/team',
 } as const;
 
 export type Routes = (typeof appRoutes)[keyof typeof appRoutes];
