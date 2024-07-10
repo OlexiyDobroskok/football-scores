6f382e43fdd8edfd1a2cb7151ad6d467882d5982
Oleksii Dobroskok <a.v.dobroskok.kh@gmail.com>
2024-07-10 15:59:25 +0300

    Add new common icons

--- src/shared/ui/icon/icon-names.ts
+++ src/shared/ui/icon/icon-names.ts
@@ -5,6 +5,9 @@ export const commonIcons = {
   ARROW_DOWN: 'arrowDown',
   ARROW_PREV: 'arrowPrev',
   ARROW_NEXT: 'arrowNext',
+  HOME: 'home',
+  FAVORITE: 'favorite',
+  USER: 'user',
 } as const;
 
 export type CommonIcons = (typeof commonIcons)[keyof typeof commonIcons];
