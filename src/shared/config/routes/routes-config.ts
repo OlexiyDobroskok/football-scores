57f96673a7e2bd6b81a297cca5d5da604e7d658b
Oleksii Dobroskok <a.v.dobroskok.kh@gmail.com>
2024-07-10 16:05:53 +0300

    Init new pages

--- src/shared/config/routes/routes-config.ts
+++ src/shared/config/routes/routes-config.ts
@@ -3,6 +3,8 @@ export const appRoutes = {
   EXPLORE: '/explore',
   MATCH: '/match',
   TEAM: '/team',
+  FAVORITE: '/favorite',
+  PROFILE: '/profile',
 } as const;
 
 export type Routes = (typeof appRoutes)[keyof typeof appRoutes];
