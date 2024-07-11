80828cdf25e263c179bbb49d0fbc8013e796516b
Oleksii Dobroskok <a.v.dobroskok.kh@gmail.com>
2024-07-11 17:34:21 +0300

    Create useScrollDirection hook

--- src/shared/utils/hooks.ts
+++ src/shared/utils/hooks.ts
@@ -11,3 +11,46 @@ export const useIsClient = (): boolean => {
 
   return isClient;
 };
+
+const scrollDirections = {
+  UP: 'up',
+  DOWN: 'down',
+  IDLE: 'idle',
+} as const;
+
+export type ScrollDirection =
+  (typeof scrollDirections)[keyof typeof scrollDirections];
+
+export const useScrollDirection = (): {
+  isScrollingUp: boolean;
+  isScrollingDown: boolean;
+  scrollDirection: ScrollDirection;
+} => {
+  const [scrollDirection, setScrollDirection] = React.useState<ScrollDirection>(
+    scrollDirections.IDLE,
+  );
+  const lastScrollY = React.useRef(0);
+
+  React.useEffect(() => {
+    const onScroll = () => {
+      const currentScrollY = window.scrollY;
+      const direction =
+        currentScrollY > lastScrollY.current
+          ? scrollDirections.DOWN
+          : scrollDirections.UP;
+
+      setScrollDirection(direction);
+      lastScrollY.current = currentScrollY;
+    };
+
+    window.addEventListener('scroll', onScroll);
+
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+
+  return {
+    isScrollingUp: scrollDirection === scrollDirections.UP,
+    isScrollingDown: scrollDirection === scrollDirections.DOWN,
+    scrollDirection,
+  };
+};
